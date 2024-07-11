6983f30e0e1e115210810be91997095ed3b243c7
unknown <johnsarker790@gmail.com>
2024-07-11 21:56:36 +0600

    first commit

--- tstOne.py
+++ tstOne.py
@@ -1,1 +1,13 @@
-print('Hello')
+# dictionary1 = {
+#     'Name': 'John',
+#     'Age' : 24,
+#     'Subject' : 'Python'
+# }
+# print(dictionary1)
+mylist = ['Hello World', 'Here','Hello World', 'Here','Hello World', 'Here']
+for x in mylist:
+    print(x)
+z = 0
+while z < len(mylist):
+    print(mylist[z])
+    z = z + 1
