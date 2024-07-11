862158cfd45082ad30855c0f4d3672da01758bdb
unknown <johnsarker790@gmail.com>
2024-07-12 02:45:25 +0600

    first commit

--- tstOne.py
+++ tstOne.py
@@ -1,13 +1,17 @@
-# dictionary1 = {
-#     'Name': 'John',
-#     'Age' : 24,
-#     'Subject' : 'Python'
-# }
-# print(dictionary1)
-mylist = ['Hello World', 'Here','Hello World', 'Here','Hello World', 'Here']
-for x in mylist:
-    print(x)
-z = 0
-while z < len(mylist):
-    print(mylist[z])
-    z = z + 1
+# Tuple
+myTuple = (1,2,3,4,5,6,7,8,9)
+print(type(myTuple))
+
+print(myTuple[:])
+x = list(myTuple)
+print(type(x))
+x.append(0)
+myTuple = tuple(x)
+print(myTuple)
+print(type(myTuple))
+
+# Unpack tuple
+(a,b,*c) = myTuple
+print(a)
+print(c)
+print(b)
